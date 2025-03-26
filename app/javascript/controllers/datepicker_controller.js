d641e7f131945ba1de888994155f5c8aa5376e2e
IllarionKopeika <larrypearson@yandex.ru>
2025-03-26 17:28:40 +0800

    responsive navbar

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -4,6 +4,7 @@ export default class extends Controller {
   connect() {
     flatpickr.localize(flatpickr.l10ns.ru)
     flatpickr(this.element, {
+      disableMobile: false,
       position: 'auto center',
       minDate: '2021-01-01',
       maxDate: new Date().fp_incr(365),

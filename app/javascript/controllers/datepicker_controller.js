f614a1d2a570f2230cbaa9e5cf72a0fe469e1a52
IllarionKopeika <larrypearson@yandex.ru>
2025-03-27 17:28:46 +0800

    minor changes

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -4,7 +4,6 @@ export default class extends Controller {
   connect() {
     flatpickr.localize(flatpickr.l10ns.ru)
     flatpickr(this.element, {
-      disableMobile: false,
       position: 'auto center',
       minDate: '2021-01-01',
       maxDate: new Date().fp_incr(365),

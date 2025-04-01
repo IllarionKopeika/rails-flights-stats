024db00a2baa67810a5931903c8124baee49b55a
IllarionKopeika <larrypearson@yandex.ru>
2025-04-01 15:31:42 +0800

    airline update active job

---
+++ app/jobs/fetch_airline_data_job.rb
@@ -0,0 +1,41 @@
+class FetchAirlineDataJob < ApplicationJob
+  require 'httparty'
+  queue_as :default
+
+  def perform(airline_id)
+    airline = Airline.find_by(id: airline_id)
+    return unless airline
+
+    url = 'https://iata-code-decoder.p.rapidapi.com/airlines'
+    headers = {
+      'x-rapidapi-key' => ENV['IATA_KEY'],
+      'x-rapidapi-host' => ENV['IATA_HOST']
+    }
+    querystring = {
+      query: airline.code
+    }
+
+    response = HTTParty.get(url, headers: headers, query: querystring)
+    if response.success?
+      data = JSON.parse(response.body, symbolize_names: true)
+      Rails.logger.debug ">>> data #{data}"
+      if data.dig(:data).blank?
+        airline.update!(
+          name: 'Unknown Airline',
+          logo_url: 'https://res.cloudinary.com/dgjzxdtrf/image/upload/v1743490823/airplane_yix0kw.png'
+        )
+      else
+        airline_data = data[:data][0]
+        airline.update!(
+          name: airline_data[:name],
+          logo_url: airline_data[:logoSymbolUrl]
+        )
+      end
+    else
+      airline.update!(
+        name: 'Unknown Airline',
+        logo_url: 'https://res.cloudinary.com/dgjzxdtrf/image/upload/v1743490823/airplane_yix0kw.png'
+      )
+    end
+  end
+end
--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,3 +1,11 @@
 class Airline < ApplicationRecord
   has_many :flights
+
+  after_create_commit :fetch_airline_data
+
+  private
+
+  def fetch_airline_data
+    FetchAirlineDataJob.perform_later(self.id)
+  end
 end

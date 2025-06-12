ac8cc9e4e07616657cc6ce0b506830249246cbf6
IllarionKopeika <larrypearson@yandex.ru>
2025-06-12 17:41:17 +0800

    continents stats

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,61 +1,80 @@
 class StatsController < ApplicationController
   def show
+    # flights
     @flights = Current.user.flights
+
+    # distance
     @total_distance = Current.user.flights.where(status: :completed).sum(:distance).round(1)
     @longest_flight_km = Current.user.flights.where(status: :completed).order(distance: :desc).first
     @shortest_flight_km = Current.user.flights.where(status: :completed).order(distance: :asc).first
     @average_distance = Current.user.flights.where(status: :completed).average(:distance).to_f.round(1)
 
+    # duration
     @total_duration = Current.user.flights.where(status: :completed).sum(:duration)
     @longest_flight_min = Current.user.flights.where(status: :completed).order(duration: :desc).first
     @shortest_flight_min = Current.user.flights.where(status: :completed).order(duration: :asc).first
     @average_duration = Current.user.flights.where(status: :completed).average(:duration).to_i
 
+    # all airports
     @airports = Current.user.flight_stats
       .where(flightstatable_type: 'Airport', role: 'general')
       .map do |stat|
         airport = Airport.find(stat.flightstatable_id)
-        [ airport.country.code, airport.code, airport.name, stat.count ]
+        [ airport.code, airport.name, stat.count ]
       end
-      .sort_by { |_, _, name, count| [ -count, name.downcase ] }
+      .sort_by { |_, name, count| [ -count, name.downcase ] }
 
+    # departure airports
     @departure_airports = Current.user.flight_stats
       .where(flightstatable_type: 'Airport', role: 'departure')
       .map do |stat|
         departure_airport = Airport.find(stat.flightstatable_id)
-        [ departure_airport.country.code, departure_airport.code, departure_airport.name, stat.count ]
+        [ departure_airport.code, departure_airport.name, stat.count ]
       end
-      .sort_by { |_, _, name, count| [ -count, name.downcase ] }
+      .sort_by { |_, name, count| [ -count, name.downcase ] }
 
+    # arrival airports
     @arrival_airports = Current.user.flight_stats
       .where(flightstatable_type: 'Airport', role: 'arrival')
       .map do |stat|
         arrival_airport = Airport.find(stat.flightstatable_id)
-        [ arrival_airport.country.code, arrival_airport.code, arrival_airport.name, stat.count ]
+        [ arrival_airport.code, arrival_airport.name, stat.count ]
+      end
+      .sort_by { |_, name, count| [ -count, name.downcase ] }
+
+    # airlines
+    @airlines = Current.user.flight_stats
+      .where(flightstatable_type: 'Airline')
+      .map do |stat|
+        airline = Airline.find(stat.flightstatable_id)
+        [ airline.logo_url, airline.code, airline.name, stat.count ]
       end
       .sort_by { |_, _, name, count| [ -count, name.downcase ] }
 
-      @airlines = Current.user.flight_stats
-        .where(flightstatable_type: 'Airline')
-        .map do |stat|
-          airline = Airline.find(stat.flightstatable_id)
-          [ airline.logo_url, airline.code, airline.name, stat.count ]
-        end
-        .sort_by { |_, _, name, count| [ -count, name.downcase ] }
+    # aircrafts
+    @aircrafts = Current.user.flight_stats
+      .where(flightstatable_type: 'Aircraft')
+      .map do |stat|
+        aircraft = Aircraft.find(stat.flightstatable_id)
+        [ aircraft.name, stat.count ]
+      end
+      .sort_by { |name, count| [ -count, name.downcase ] }
 
-      @aircrafts = Current.user.flight_stats
-        .where(flightstatable_type: 'Aircraft')
-        .map do |stat|
-          aircraft = Aircraft.find(stat.flightstatable_id)
-          [ aircraft.name, stat.count ]
-        end
-        .sort_by { |name, count| [ -count, name.downcase ] }
+    # countries
+    @countries = Current.user.visits
+      .where(visitable_type: 'Country')
+      .map do |visit|
+        country = Country.find(visit.visitable_id)
+        [ country.code, country.name ]
+      end
 
-      @countries = Current.user.visits
-        .where(visitable_type: 'Country')
-        .map do |visit|
-          country = Country.find(visit.visitable_id)
-          [ country.code, country.name ]
-        end
+    # continents
+    visited_region_ids = Current.user.visits
+      .where(visitable_type: 'Region')
+      .pluck(:visitable_id)
+      .to_set
+    @regions = Region.all.map do |region|
+      [ region.name, visited_region_ids.include?(region.id) ]
+    end
   end
 end
